46bf9c69bc7ab047f79d4f43e2cec7584c4a1eea
Adonteng Sarkodee <yourgithubemail@example.com>
2025-09-14 02:20:11 +0000

    Task 5: print first argument as integer or Not a number

---
+++ 5-to_integer.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const args = process.argv.slice(2);
+const firstArg = args[0];
+
+const num = parseInt(firstArg);
+
+if (!isNaN(num)) {
+  console.log("My number: " + num);
+} else {
+  console.log("Not a number");
+}
